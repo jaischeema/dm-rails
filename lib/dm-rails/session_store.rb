fca4f08b26b032098cc7994683cc78c00edbdc74
jaischeema <jaischeema@gmail.com>
2012-07-06 14:08:50 +1000

    Add session fix from the 1.3.0.beta

--- lib/dm-rails/session_store.rb
+++ lib/dm-rails/session_store.rb
@@ -13,7 +13,7 @@ module Rails
         include ::DataMapper::Resource
 
         property :id,         Serial
-        property :session_id, String,   :required => true, :unique => true
+        property :session_id, String,   :required => true, :unique => true, :length => 0..150
         property :data,       Object,   :required => true
         property :updated_at, DateTime,                    :index => true
 
@@ -27,7 +27,8 @@ module Rails
 
       end
 
-      SESSION_RECORD_KEY = 'rack.session.record'.freeze
+      ENV_SESSION_OPTIONS_KEY = ::Rack::Session::Abstract::ENV_SESSION_OPTIONS_KEY unless const_defined?("ENV_SESSION_OPTIONS_KEY")
+      SESSION_RECORD_KEY      = 'rack.session.record'.freeze
 
       class_attribute :session_class
       self.session_class = Session
@@ -41,7 +42,7 @@ module Rails
         [ sid, session.data ]
       end
 
-      def set_session(env, sid, session_data)
+      def set_session(env, sid, session_data, options = {})
         session            = get_session_resource(env, sid)
         session.data       = session_data
         session.updated_at = DateTime.now if session.dirty?
@@ -59,9 +60,14 @@ module Rails
       def find_session(sid)
         self.class.session_class.first_or_new(:session_id => sid)
       end
+      
+      def destroy_session(env, sid = nil, options = {})
+        sid ||= current_session_id(env)
+        find_session(sid).destroy
+      end
 
       def destroy(env)
-        find_session(current_session_id(env)).destroy
+        destroy_session(env)
       end
 
     end
